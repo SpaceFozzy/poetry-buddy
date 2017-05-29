03f932251c4dea1dc1e0026f7024eee1da5d2e5d
Jon Richardson <jon@robotmonster.ca>
2017-05-29 07:43:31 -0400

    Add unit tests for rhyme suggestion user feedback

--- src/app/poem-couplet/rhyme-suggestion/rhyme-suggestion.component.spec.ts
+++ src/app/poem-couplet/rhyme-suggestion/rhyme-suggestion.component.spec.ts
@@ -27,5 +27,56 @@ describe('RhymeSuggestionComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  
+
+  it('should give the user feedback when no rhymes are loaded', () => {
+    component.searchText = "";
+    fixture.detectChanges();
+
+    let element = fixture.debugElement.query(By.css('#before-suggestions'))
+    expect(element).toBeTruthy();
+  });
+
+  it('should show the rhyme list when not loading and hide it when it is loading', () => {
+    component.isLoading = false;
+    fixture.detectChanges();
+
+    let element = fixture.debugElement.query(By.css('#rhyme-list'))
+    expect(element).toBeTruthy();
+
+    component.isLoading = true;
+    fixture.detectChanges();
+
+    element = fixture.debugElement.query(By.css('#rhyme-list'))
+    expect(element).toBeFalsy();
+  });
+
+  it('should give the user feedback when loading rhymes', () => {
+    component.searchText = "test";
+    component.isLoading = true;
+    fixture.detectChanges();
+
+    let element = fixture.debugElement.query(By.css('#loading-rhymes'))
+    expect(element).toBeTruthy();
+  });
+
+  it('should give the user feedback when no rhymes are found', () => {
+    component.currentWord = "test"
+    component.isLoading = false;
+    component.rhymeHints = [];
+    fixture.detectChanges();
+
+    let element = fixture.debugElement.query(By.css('#no-rhymes-found'))
+    expect(element).toBeTruthy();
+  });
+
+  it('should give the user feedback when rhymes are found', () => {
+    component.currentWord = "test"
+    component.isLoading = false;
+    component.rhymeHints = ["best"];
+    fixture.detectChanges();
+
+    let element = fixture.debugElement.query(By.css('#rhymes-found'))
+    expect(element).toBeTruthy();
+  });
+
 });
