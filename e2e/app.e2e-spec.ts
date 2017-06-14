fb69980b559b82389d110a195a2dc42543679363
Jon Richardson <jon@robotmonster.ca>
2017-06-13 23:14:39 -0400

    Add e2e test to check that rhymes load when one line is typed

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,28 @@
-import { PoeticPage } from './app.po';
+import { PoetryBuddyPage } from './app.po';
+import { element, by, browser } from "protractor";
+
 
 describe('poetic App', () => {
-  let page: PoeticPage;
+  let page: PoetryBuddyPage;
 
   beforeEach(() => {
-    page = new PoeticPage();
+    page = new PoetryBuddyPage();
   });
 
   it('should display a header saying Poetry Buddy', () => {
     page.navigateTo();
     expect(page.getHeaderText()).toEqual('Poetry Buddy');
+  });
+
+  it('should show a list of rhyme options when a word is typed on the first line', () => {
+    page.navigateTo();
+    let firstLineInput = element.all(by.css("input")).first();
+    firstLineInput.sendKeys("The first line ends in dog");
+    browser.driver.sleep(4000);
+
+    expect(element(by.id('rhymes-found')).isPresent()).toBe(true);
+    
+    let listItemCollection = element.all(by.css("#rhyme-list li"));
+    expect(listItemCollection.count()).toBeGreaterThan(3);
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,6 @@
 import { browser, element, by } from 'protractor';
 
-export class PoeticPage {
+export class PoetryBuddyPage {
   navigateTo() {
     return browser.get('/');
   }
