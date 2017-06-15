dd844cc7c8cafe995365e02988a8ced551601e13
Jon Richardson <jon@robotmonster.ca>
2017-06-15 19:24:43 -0400

    Add e2e test for no rhyme suggestion feedback

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -14,6 +14,15 @@ describe('poetic App', () => {
     expect(page.getHeaderText()).toEqual('Poetry Buddy');
   });
 
+  it('should alert the user when no rhyme suggestions are found', () => {
+    page.navigateTo();
+    let firstLineInput = element.all(by.css("input")).first();
+    firstLineInput.sendKeys("Thefirstlineendsindog");
+    browser.driver.sleep(4000);
+
+    expect(element(by.id('no-rhymes-found')).isPresent()).toBe(true);
+  });
+
   it('should fetch and display a list of rhyme options when a word is typed on the first line', () => {
     page.navigateTo();
     let firstLineInput = element.all(by.css("input")).first();
@@ -25,4 +34,6 @@ describe('poetic App', () => {
     let listItemCollection = element.all(by.css("#rhyme-list li"));
     expect(listItemCollection.count()).toBeGreaterThan(3);
   });
+
+
 });
