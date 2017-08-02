024d293bbc0b4204fd66709155d3a7555eade85b
Jon Richardson <jon@robotmonster.ca>
2017-08-01 21:42:24 -0400

    Remove redundant comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';  // <-- #1 import module
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
 
 import { SweetAlert2Module } from '@toverux/ngsweetalert2';
